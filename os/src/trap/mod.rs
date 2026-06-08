b8d51ca5a703ced30d76638468b7c14f870f86d6
l <2950851840@qq.com>
2026-06-08 15:32:11 +0000

    实验5完成 分时多任务和抢占式调度操作系统 学号：23301100

--- os/src/trap/mod.rs
+++ os/src/trap/mod.rs
@@ -2,14 +2,8 @@ use core::arch::global_asm;
 
 global_asm!(include_str!("trap.S"));
 
-pub fn init() {
-    unsafe extern "C" { fn __alltraps(); }
-    unsafe {
-        stvec::write(__alltraps as *const () as usize, TrapMode::Direct);
-    }
-}
-
 mod context;
+pub use context::TrapContext;
 
 use riscv::register::{
     mtvec::TrapMode,
@@ -18,37 +12,66 @@ use riscv::register::{
         self,
         Trap,
         Exception,
+        Interrupt,
     },
     stval,
+    sie,
 };
 
 use crate::syscall::syscall;
+use crate::task::{
+    exit_current_and_run_next,
+    suspend_current_and_run_next,
+};
+use crate::timer::set_next_trigger;
 
+pub fn init() {
+    unsafe extern "C" {
+        fn __alltraps();
+    }
+    unsafe {
+        stvec::write(__alltraps as usize, TrapMode::Direct);
+    }
+}
+
+pub fn enable_timer_interrupt() {
+    unsafe {
+        sie::set_stimer();
+    }
+}
 
 #[unsafe(no_mangle)]
 pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
     let scause = scause::read();
     let stval = stval::read();
+
     match scause.cause() {
         Trap::Exception(Exception::UserEnvCall) => {
             cx.sepc += 4;
             cx.x[10] = syscall(cx.x[17], [cx.x[10], cx.x[11], cx.x[12]]) as usize;
         }
+
         Trap::Exception(Exception::StoreFault) |
         Trap::Exception(Exception::StorePageFault) => {
             println!("[kernel] PageFault in application, core dumped.");
-//            run_next_app();
+            exit_current_and_run_next();
         }
+
         Trap::Exception(Exception::IllegalInstruction) => {
             println!("[kernel] IllegalInstruction in application, core dumped.");
-  //          run_next_app();
+            exit_current_and_run_next();
         }
+
+        // 时钟中断 → 抢占式调度（实验5）
+        Trap::Interrupt(Interrupt::SupervisorTimer) => {
+            set_next_trigger();
+            suspend_current_and_run_next();
+        }
+
         _ => {
             panic!("Unsupported trap {:?}, stval = {:#x}!", scause.cause(), stval);
         }
     }
+
     cx
 }
-
-pub use context::TrapContext;
-
